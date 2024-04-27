e76861a66e53b4b59f4f27a581882b898264072f
VorobeiIvan <0675361905vi@gmail.com>
2024-04-27 10:24:23 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { program } from "commander";
+import colors from "colors";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,27 +18,34 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const contacts = await listContacts();
+      console.log(colors.bgGreen("Contacts list:"));
+      console.table(contacts);
       break;
 
     case "get":
-      // ... id
+      const contact = await getContactById(id);
+      console.log(colors.bgGreen("Contact:"));
+      console.table(contact);
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await addContact(name, email, phone);
+      console.log(colors.bgGreen("Contact added!"));
+      console.table(newContact);
       break;
 
     case "remove":
-      // ... id
+      const deletedContact = await removeContact(id);
+      console.log(colors.bgRed("Contact deleted!"));
+      console.table(deletedContact);
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn(colors.bgRed("Unknown action type!"));
   }
 }
 
